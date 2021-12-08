6ede8e60df7215aee7ef4129d0a2601e1cb35758
Emmanuel Valdez <evaldez89@gmail.com>
2021-12-08 23:18:09 +0000

    [ADD] top services report print method

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,4 +1,4 @@
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
@@ -10,3 +10,19 @@ class SaleOrder(models.Model):
         rquired=True,
         domain="[('owner_id','=',partner_id)]",
     )
+
+    def print_top_services(self):
+        self.env['account.move.line']
+        product_ids = self.env['product.product'].search(
+            [
+                (
+                    'categ_id',
+                    '=',
+                    self.env.ref('car_workshop.product_category_repair_service').id,
+                )
+            ]
+        )
+
+        return self.env.ref('car_workshop.report_car_workshop_products').report_action(
+            [], data={'name': 'Emmanuel Valdez', 'extras': {'age': 32}}
+        )
